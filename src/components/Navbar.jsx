2665d0b18fc7f68fe3d4379b0c545cd6ac0993ba
tijanicica <tijanapetrovicns03@gmail.com>
2025-04-24 15:01:39 +0200

    finished home page without photo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,61 @@
 import {NavLink} from "react-router";
 import logo from "/src/assets/logo-removebg-preview.png";
+import { Menu, X } from 'lucide-react';
+import {useState} from "react";
 
 const Navbar = () => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
 
-        <nav className="flex items-center justify-between bg-accent px-6 py-6 fixed top-0 w-full z-[1000] shadow-md">
-            <div className="flex items-center">
-                <img src={logo} alt="Logo"  className="h-[50px]" />
+        <nav className="flex flex-wrap items-center justify-between bg-navbar px-6 py-4 md:py-6 fixed top-0 w-full z-[1000] shadow-md">
+            <div className="flex justify-between items-center w-full md:w-auto">
+                <img src={logo} alt="Logo" className="h-[40px] md:h-[50px]" />
+
+                <button
+                    className="md:hidden text-[var(--default-font-color)] hover:text-[var(--font-hover)]"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {isOpen ? <X size={20} /> : <Menu size={20} />}
+                </button>
             </div>
 
-            <div className="flex gap-6 mr-20 font-[350] text-textMain no-underline hover:text-textHover">
-                <NavLink to='/' >Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/skills">Skills</NavLink>
-                <NavLink to="/projects">Projects</NavLink>
-                <NavLink to="/contacts">Contact</NavLink>
+            <div className="hidden md:flex md:flex-row gap-6 text-center font-[350] no-underline">
+                <NavLink to='/' className="hover:text-[var(--font-hover)]">Home</NavLink>
+                <NavLink to='/about' className="hover:text-[var(--font-hover)]">About</NavLink>
+                <NavLink to='/skills' className="hover:text-[var(--font-hover)]">Skills</NavLink>
+                <NavLink to='/projects' className="hover:text-[var(--font-hover)]">Projects</NavLink>
+                <NavLink to='/contacts' className="hover:text-[var(--font-hover)]">Contact</NavLink>
             </div>
 
+
+            {isOpen && (
+                <div
+                    className="fixed inset-0 bg-opacity-30 z-[998]"
+                    onClick={() => setIsOpen(false)}
+                >
+                    <div className="flex flex-col fixed gap-4 top-0 right-0 h-[300px] w-[200px] bg-navbar shadow-lg p-6 text-center z-[999] transition-all duration-300 ease-in-out"
+                         onClick={(e) => e.stopPropagation()}
+                    >
+                        <button
+                            className="mb-4 self-end text-[var(--default-font-color)] hover:text-[var(--font-hover)]"
+                            onClick={() => setIsOpen(false)}
+                        >
+                            <X size={20} />
+                        </button>
+
+                    <NavLink to='/' className="hover:text-[var(--font-hover)]" onClick={() => setIsOpen(false)}>Home</NavLink>
+                    <NavLink to='/about' className="hover:text-[var(--font-hover)]" onClick={() => setIsOpen(false)}>About</NavLink>
+                    <NavLink to='/skills' className="hover:text-[var(--font-hover)]" onClick={() => setIsOpen(false)}>Skills</NavLink>
+                    <NavLink to='/projects' className="hover:text-[var(--font-hover)]" onClick={() => setIsOpen(false)}>Projects</NavLink>
+                    <NavLink to='/contacts' className="hover:text-[var(--font-hover)]" onClick={() => setIsOpen(false)}>Contact</NavLink>
+                </div>
+                    </div>
+            )}
         </nav>
+
+
     );
 };
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,61 @@
+import plane from "../assets/plane.jpeg"
+
 const HomePage = () => {
     return (
-        <body className=" font-display text-[rgba(236,228,217,0.87)] min-w-[320px] min-h-screen m-0">
-        <div className="flex min-h-screen flex-row min-w-[320px]">
-            <div className="flex flex-col items-center justify-between pt-12 pr-8 pb-8 pl-16 mt-28 mb-20 italic text-textMain">
-                <span>2021</span>
-                <div  className="flex-grow-[0.9] w-[2px] bg-accent"></div>
-                <span>Present</span>
-            </div>
-            <div className="absolute left-40 top-[208px] w-1/2 bg-timelineLine p-4 px-8 min-h-[51vh] animate-fadeInLeft">
-                <h2 className="font-display font-light text-white ml-52 text-[1.5rem]">welcome to my</h2>
-                <h3 className="text-[7rem] font-light text-textMain">PORTFOLIO</h3>
-            </div>
-        </div>
-       </body>
+        <div className="font-display min-w-[320px] max-w-[80%] min-h-screen m-0 flex items-center justify-center lg:justify-start mx-auto">
 
+
+            <div className="fixed top-1/2 left-10 transform -translate-y-1/2 flex flex-col items-center justify-between h-1/2 italic z-10">
+                <span className="text-sm md:text-base">2021</span>
+                <div className="w-[2px] flex-grow bg-navbar my-2"></div>
+                <span className="text-sm md:text-base">Present</span>
+            </div>
+
+
+            <div className="min-h-screen flex flex-col items-center justify-center relative">
+
+
+                <div className="w-[90%] min-h-[51vh] flex flex-col justify-center p-4 md:p-8 gap-2 items-center">
+
+                    <h2 className="font-light text-[1rem] md:text-[1.25rem] uppercase tracking-widest">
+                        Welcome to my
+                    </h2>
+
+                    <h3 className="font-extralight text-[var(--color-portfolio)] leading-none
+                                   text-[3rem] sm:text-[4.5rem] md:text-[6rem] lg:text-[8rem] -tracking-tighter">
+                        PORTFOLIO
+                    </h3>
+
+                    <h4 className="font-light text-[var(--color-welcome)] text-[1rem] md:text-[1.25rem] uppercase tracking-widest md:text-left">
+                        by
+                    </h4>
+
+                    <h5
+                        className="font-light text-[var(--color-welcome)] text-[1.75rem] md:text-[2.5rem] tracking-widest"
+                        style={{ fontFamily: 'Parisienne, cursive' }}
+                    >
+                        Tijana
+                    </h5>
+
+                </div>
+            </div>
+
+            <div
+                className="
+                    fixed bottom-6 left-1/2 -translate-x-1/2 text-center
+                    lg:left-auto lg:right-20 lg:bottom-25 lg:pr-4 lg:translate-x-0 lg:text-center
+                "
+            >
+                <p className="tracking-widest text-[var(--color-welcome)] text-sm ">
+                    just a girl in a tech world
+                </p>
+                <p className="tracking-widest text-[var(--color-navbar)] text-sm">
+                    scroll for more
+                </p>
+            </div>
+
+
+        </div>
     );
 };
 
